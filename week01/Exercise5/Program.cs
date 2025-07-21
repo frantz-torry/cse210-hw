bcc5d941a5476fb71b9462355c038ea0e014dbb5
frantz-torry <frantztorry@gmail.com>
2025-07-21 19:58:28 -0400

    Week 01 - With Exo 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,35 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the Program!");
-    }
-    static string PromptUserName(string name)
-    {
-        Console.Write("What is your name? ");
-        name = Console.ReadLine();
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName(string name)
+        {
+            Console.Write("What is your name? ");
+            name = Console.ReadLine();
 
-        return name;
+            return name;
 
-    }
-    static int PromptUserNumbet(int number)
-    {
-        Console.Write("Please enter your favorite number: ");
-        string num = Console.ReadLine();
-        number = int.Parse(num);
+        }
+        static int PromptUserNumbet(int number)
+        {
+            Console.Write("Please enter your favorite number: ");
+            string num = Console.ReadLine();
+            number = int.Parse(num);
 
-        return number;
-    }
-    static int SquareNumber(int number)
-    {
-        int square = number * number;
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
 
-        return square;
-    }
-    static void DisplayResult(string name, int square)
-    {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
     }
 }
+
