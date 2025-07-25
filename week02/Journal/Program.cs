10c045a9c2cab946e0e8ef91817295cc70c592a4
frantz-torry <frantztorry@gmail.com>
2025-07-25 18:38:10 -0400

    Week 02 - Project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string randomPrompt = promptGenerator.GetRandomPrompt();
+        Console.WriteLine(randomPrompt);
     }
 }
+
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+
+        Resume myResume = new Resume();
+        myResume._name = "Jack Sparrow";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
